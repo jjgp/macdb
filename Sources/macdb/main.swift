efe099ea890bca8fbdf74bcd71dd944b1097bc47
Jason Prasad <jasongprasad@gmail.com>
2019-11-19 21:12:24 -0500

    taking a single screenshot of the Simulator window

--- Sources/macdb/main.swift
+++ Sources/macdb/main.swift
@@ -1,1 +1,16 @@
-print("Hello, world!")
+import Foundation
+
+let windowList = CGWindowListCopyWindowInfo(.optionOnScreenOnly, kCGNullWindowID) as? [[String : Any]]
+let simulatorWindowInfo = windowList?.first { $0[String(kCGWindowOwnerName)] as? String == "Simulator" }
+let simulatorWindowID = simulatorWindowInfo?[String(kCGWindowNumber)] as? CGWindowID
+let windowImageRef = simulatorWindowID.map {
+    CGWindowListCreateImage(.null, .optionIncludingWindow, $0, .boundsIgnoreFraming)
+}
+
+signal(SIGINT) { _ in
+    exit(0)
+}
+
+autoreleasepool {
+    RunLoop.main.run()
+}
